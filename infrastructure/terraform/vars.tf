ad8cd43d0dd071354f9ee541b1168ef5bb89b072
Shahab Dogar <shahab96@gmail.com>
2022-07-07 20:59:33 -0700

    fix: Add var for region and default to us-west1

--- infrastructure/terraform/vars.tf
+++ infrastructure/terraform/vars.tf
@@ -6,12 +6,16 @@ variable "project" {
   description = "Your Google project ID"
 }
 
-variable "region" {
-  type = string
-  description = "The region in GCP to deploy to"
-}
-
 variable "zone_id" {
   type = string
   description = "The zone ID in Cloudflare to deploy to"
+}
+
+#------------------------
+# Configurable variables
+#------------------------
+variable "region" {
+  type = string
+  description = "The region in GCP to deploy to"
+  default = "us-west1"
 }
