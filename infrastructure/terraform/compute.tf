4e56b62b77cb43e293a354f4d221df2ba05435f8
Shahab Dogar <shahab96@gmail.com>
2022-07-07 23:30:46 -0700

    fix: Remove module and api

--- infrastructure/terraform/compute.tf
+++ infrastructure/terraform/compute.tf
@@ -50,29 +50,3 @@ resource "google_compute_firewall" "this" {
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["minecraft"]
 }
-
-module "agent_policy" {
-  source     = "terraform-google-modules/cloud-operations/google//modules/agent-policy"
-  version    = "0.2.3"
-
-  project_id = var.project
-  policy_id  = "ops-agent"
-  agent_rules = [
-    {
-      type               = "ops-agent"
-      version            = "current-major"
-      package_state      = "installed"
-      enable_autoupgrade = true
-    },
-  ]
-  group_labels = [
-    {
-      env = "minecraft"
-    }
-  ]
-  os_types = [
-    {
-      short_name = "ubuntu"
-    }
-  ]
-}
