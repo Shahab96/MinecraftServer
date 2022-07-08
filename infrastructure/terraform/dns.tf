7db78b296ec4d53c5f24ba662e2e7f69d3bd4f71
Shahab Dogar <shahab96@gmail.com>
2022-07-07 20:56:01 -0700

    fix: Set cloudflare provider on dns record

--- infrastructure/terraform/dns.tf
+++ infrastructure/terraform/dns.tf
@@ -1,4 +1,6 @@
 resource "cloudflare_record" "this" {
+  provider = cloudflare
+
   zone_id = var.zone_id
   name = "atm6.shahab96.com"
   value = google_compute_instance.this.network_interface.0.access_config.0.nat_ip
