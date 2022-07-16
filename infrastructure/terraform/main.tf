33719fdb784559709a90320ff99ca706bc3fb751
Shahab Dogar <shahab96@gmail.com>
2022-07-16 12:35:03 -0700

    fix: Remove google provider

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.27.0"
-    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "3.18.0"
@@ -15,13 +11,6 @@ terraform {
   }
 }
 
-provider "google" {
-  project = var.project
-  region = var.region
-  zone = var.gcp_zone
-}
-
-provider "cloudflare" {
-}
+provider "cloudflare" {}
 
 provider "linode" {}
