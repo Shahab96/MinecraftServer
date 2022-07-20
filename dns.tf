5e6f29e1c196fa565f35864b991d9e6a924f81ad
Shahab Dogar <shahab96@gmail.com>
2022-07-19 21:40:55 -0700

    feat: Remove DNS record from cloudflare

--- dns.tf
+++ dns.tf
@@ -2,14 +2,6 @@ data "google_dns_managed_zone" "this" {
   name = "dogar-dev"
 }
 
-resource "cloudflare_record" "this" {
-  zone_id = var.zone_id
-  name = "atm6.shahab96.com"
-  value = linode_instance.this.ip_address
-  type = "A"
-  ttl = 60
-}
-
 resource "google_dns_record_set" "this" {
   name = "atm6.${data.google_dns_managed_zone.this.dns_name}"
   type = "A"
--- vars.tf
+++
@@ -1,7 +0,0 @@
-#------------------------
-# Required variables
-#------------------------
-variable "zone_id" {
-  type = string
-  description = "The zone ID in Cloudflare to deploy to"
-}
